4749786ebde8a6283039a274414f174392fab1d4
DimitarChristoff <christoff@gmail.com>
2012-07-13 14:12:42 +0100

    small refactors and comments in code

--- epitome/js/app.js
+++ epitome/js/app.js
@@ -4,34 +4,42 @@
 	var App = window.App;
 
 	// Your starting point. Enjoy the ride!
-	var collection = new App.TodoCollection(null, {
+	var todos = new App.TodoCollection(null, {
+		// a consistent collection if is needed if you want to use storage for a collection.
 		id: 'todos'
 	});
 
 	// populate from storage if available
-	collection.setUp(collection.retrieve());
+	todos.setUp(todos.retrieve());
 
+	// instantiate the todo list view
 	App.todoView = new App.TodoView({
 
-		collection: collection,
+		// bind to the collection and its events and model events
+		collection: todos,
 
-		model: App.Todo,
-
+		// encapsulating element to bind to
 		element: document.id('todo-list'),
 
+		// template to use
 		template: document.id('item-template').get('text')
 
-
 	});
 
+	// the main view is for the footer/stats/controls
 	App.mainView = new App.MainView({
 
-		collection: collection,
+		// also bound to the same collection but with a different output logic.
+		collection: todos,
 
-		element: document.id('todoapp')
+		// encapsulating element to bind to
+		element: document.id('todoapp'),
+
+		// stats template from DOM
+		template: document.id('stats-template').get('text')
 	});
 
-
+	// the pseudo controller via Epitome.Router
 	App.router = new Epitome.Router({
 		routes: {
 			'': 'init',
@@ -40,13 +48,19 @@
 		},
 
 		onInit: function() {
+			// we want to always have a state
 			this.navigate('#!/');
 		},
 
 		onApplyFilter: function(filter) {
-			collection.filterType = filter || false;
+			// the filter is being used by the todo collection and view.
+			// when false, the whole collection is being passed.
+			todos.filterType = filter || false;
+
+			// render as per current filter
 			App.todoView.render();
 
+			// fix up the links quickie.
 			var self = this;
 			document.getElements('#filters li a').each(function(link) {
 				link.set('class', link.get('href') == self.req ? 'selected' : '');
--- epitome/js/views/todo-main.js
+++ epitome/js/views/todo-main.js
@@ -3,12 +3,13 @@
 
 	window.App = window.App || {};
 
-	// main view encapsulating the app itself.
 	App.MainView = new Class({
+		// main view (presenter) encapsulating the app itself.
 
 		Extends: Epitome.View,
 
 		options: {
+			// eavesdrop on these events
 			events: {
 				'change:relay(#new-todo)': 'addTodo',
 				'keypress:relay(#new-todo)': 'handleKeypress',
@@ -17,6 +18,7 @@
 				'click:relay(#filters.a)': 'setFilters'
 			},
 
+			// pass on some options for later func
 			newTodo: 'new-todo',
 
 			footer: 'footer',
@@ -26,6 +28,7 @@
 			toggleAll: 'toggle-all',
 
 			onToggleAll: function(e, el) {
+				// all todos will change their models to the new completed value
 				var state = el.get('checked') ? 'completed' : 'active';
 				this.collection.each(function(model) {
 					model.set('completed', state);
@@ -33,44 +36,53 @@
 			},
 
 			onHandleKeypress: function(e, el) {
+				// on enter, submit.
 				if (e.key == 'enter')
 					this.addTodo();
 
+				// om esc, reset
 				if (e.key == 'esc')
 					this.newTodo.set('value', '').blur();
 			},
 
 			onClearCompleted: function() {
-				// becasue removing a model reindexes, cannot apply that in a normal loop.
+				// because removing a model re-indexes so we don't get a sparse array, cannot apply that in a normal loop.
 				var toRemove = this.collection.filter(function(model) {
 					return model.get('completed') == 'completed';
 				});
 
-				// grab individual models and remove them.
+				// removeModel actually supports a single model or an array of models as arguments.
 				this.collection.removeModel(toRemove);
 				this.render();
 			},
 
 			onAddTodo: function() {
+				// go to method
 				this.addTodo();
 			},
 
 			'onChange:collection': function() {
+				// also, re-render on change of collection
 				this.render();
-			},
+			}
 
-			template: document.id('stats-template').get('text')
 		},
 
 		initialize: function(options) {
+			// call default view constructor.
 			this.parent(options);
+
+			// store some pointers to static elements
 			this.newTodo = document.id(this.options.newTodo);
 			this.footer = document.id(this.options.footer);
 			this.toggleAll = document.id(this.options.toggleAll);
+
+			// draw it.
 			this.render();
 		},
 
 		addTodo: function() {
+			// adding a new model when data exists
 			var val = this.newTodo.get('value').trim();
 
 			if (val.length) {
@@ -80,10 +92,14 @@
 				});
 			}
 
+			// clear the input
 			this.newTodo.set('value', '');
 		},
 
 		render: function() {
+			// main method to output everything. well. the footer anyway.
+
+			// work out what we have remaining and what is complete
 			var remaining = 0,
 				completed = this.collection.filter(function(el) {
 					var status = el.get('completed') == 'completed';
@@ -93,13 +109,14 @@
 					return status;
 				}).length;
 
+			// output footer
 			this.footer.set('html', this.template({
 				completed: completed,
 				remaining: remaining
 			}));
 
+			// auto-correct the toggle-all checkbox with the new stats.
 			this.toggleAll.set('checked', this.collection.length ? !remaining : false);
-
 		}
 
 	});
